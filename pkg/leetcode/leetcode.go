3c2332cd26e4b445684af5958790690bc8878b75
PPsteven <ppsteven@outlook.com>
2024-04-14 13:13:45 +0800

    feat: tags

--- pkg/leetcode/leetcode.go
+++ pkg/leetcode/leetcode.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/zcong1993/leetcode-tool/internal/config"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -12,6 +13,8 @@ import (
 
 	"github.com/tidwall/gjson"
 )
+
+const RemoteProblems = "https://raw.githubusercontent.com/PPsteven/leetcode-tool/master/data/problems.json"
 
 type Meta struct {
 	Index      string
@@ -47,10 +50,43 @@ func NewLeetcode(config *config.Config) *Leetcode {
 	return &Leetcode{Config: config}
 }
 
+func DownloadFile(remoteFile string) error {
+	// Create the file
+	out, err := os.Create("data/problems.json")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(remoteFile)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (l *Leetcode) getAllProblem() ([]byte, error) {
-	file, err := ioutil.ReadFile("/Users/ppsteven/Projects/leetcode-tool/data/problems.json")
-	if err == os.ErrNotExist {
-		return nil, errors.New("234324")
+	file, err := ioutil.ReadFile("data/problems.json")
+	if err != nil && errors.Is(err, os.ErrNotExist) {
+		fmt.Println(fmt.Sprintf("file problems.json not exists, start downloading from %s", RemoteProblems))
+		err = DownloadFile(RemoteProblems)
+
+		if err != nil {
+			return nil, fmt.Errorf("download file failed: %v", err)
+		}
+		file, err := ioutil.ReadFile("data/problems.json")
+		if err != nil {
+			return nil, fmt.Errorf("read file failed: %v", err)
+		}
+		return file, nil
 	}
 	return file, nil
 }
@@ -97,16 +133,21 @@ func (l *Leetcode) GetMetaByNumber(number string) (*Meta, error) {
 }
 
 func (l *Leetcode) GetTags() ([]Tag, error) {
-	resp, err := http.Get("https://leetcode-cn.com/problems/api/tags/")
-	if err != nil {
-		return nil, err
+	if l.Problems == nil {
+		l.Problems, _ = l.getAllProblem()
 	}
-	defer resp.Body.Close()
-	bt, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
+
+	tags := make([]Tag, 0)
+	tagsMap := make(map[string]Tag)
+	for _, problem := range gjson.ParseBytes(l.Problems).Map() {
+		_ = json.Unmarshal([]byte(problem.Get("topicTags").Raw), &tags)
+		for _, tag := range tags {
+			tagsMap[tag.Slug] = tag
+		}
 	}
-	res := make([]Tag, 0)
-	err = json.Unmarshal([]byte(gjson.GetBytes(bt, "topics").Raw), &res)
-	return res, err
+	tags = make([]Tag, 0, len(tagsMap))
+	for _, tag := range tagsMap {
+		tags = append(tags, tag)
+	}
+	return tags, nil
 }
